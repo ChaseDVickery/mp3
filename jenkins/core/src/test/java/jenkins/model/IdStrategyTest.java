ddddd5542cb800918c614352dd17429a2211ee2f
Chase Vickery <chase.vickery@uky.edu>
2020-10-28 13:56:45 -0400

    quick file rename (from q4)

--- jenkins/core/src/test/java/jenkins/model/IdStrategyTest.java
+++ jenkins/core/src/test/java/jenkins/model/IdStrategyTest.java
@@ -7,13 +7,14 @@ import static org.junit.Assert.*;
 import org.junit.Rule;
 import org.junit.rules.TemporaryFolder;
 import org.jvnet.hudson.test.Issue;
+import CaseInsensitive;
 
 public class IdStrategyTest {
 
     @Test
     public void testidFromFilenameBasic() {
         String filename = "abcXYZ_. -";
-        String id = CaseInsensitive.idFromFilename(filenam);
+        String id = CaseInsensitive.idFromFilename(filename);
         assertEquals(filename, id);
     }
 
